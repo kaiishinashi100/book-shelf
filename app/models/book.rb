5343ee8f49d2723e5092142d4f9334b42f450f14
kaiishinashi100 <70202941+kaiishinashi100@users.noreply.github.com>
2020-10-14 15:24:27 +0900

    イメージ画像登録機能実施

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,10 @@
 class Book < ApplicationRecord
   has_many   :comments
   belongs_to :user
+  has_one_attached :image
+
+  with_options presence: true do
+    validates  :image
+    validates  :title
+  end  
 end
