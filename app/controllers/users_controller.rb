303f20028a792d333107401dadc027d23b6fdcfa
kaiishinashi100 <70202941+kaiishinashi100@users.noreply.github.com>
2020-10-17 10:59:17 +0900

    部分テンプレートで本棚の記述をまとめる

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,5 +2,11 @@ class UsersController < ApplicationController
 
   def index
   end
+
+  def show
+    @books = Book.all
+    @book = Book.new
+    @user = User.find(params[:id])
+  end
   
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: 'users#index'
-  resources :users, only: [:index]
   resources :books, only: [:index, :new, :create, :show] do
     resources :comments, only: :create;
   end
+  resources :users, only: [:index, :show]
 end
